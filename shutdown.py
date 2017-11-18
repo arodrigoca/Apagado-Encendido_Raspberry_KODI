38df92c155548daf8bd7dec369685adfa0948cbc
miguelangel3 <miguelangel.abb@gmail.com>
2017-11-18 21:35:17 +0100

    Cambio pin al 05 que es donde tengo conectado el pulsador

--- shutdown.py
+++ shutdown.py
@@ -5,10 +5,10 @@ import time
 import os
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(03, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(05, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 while True:
-    print GPIO.input(03)
-    if(GPIO.input(03) == False):
+    print GPIO.input(05)
+    if(GPIO.input(05) == False):
         os.system("sudo shutdown -h now")
         break
     time.sleep(1)
